9bacfec1cb9254680d76dd70c31479a6ede06a80
indroji-mondal <indro.cse.bu@gmail.com>
2024-12-14 08:40:41 +0600

    Addd query

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import useAuth from '../../hooks/useAuth';
+import { useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const JobApply = () => {
+    const {user}= useAuth();
+    const {id}= useParams();
     const handleApply = (e)=>{
         e.preventDefault();
         const form = e.target;
@@ -8,19 +13,44 @@ const JobApply = () => {
         const github = form.github.value;
         const resume = form.resume.value;
         console.log(linkedIn, github, resume);
+        const jobApplication ={
+            job_id: id,
+            applicant_email: user.email,
+            linkedIn,
+            github,
+            resume
+        }
+        fetch('http://localhost:5000/job-applications',{
+            method: 'POST',
+            headers :{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(jobApplication)
+
+        })
+        .then(res=> res.json())
+        .then(data=> {
+            if(data.insertedId){
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Your application has been saved",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
         
     }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Apply now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                {/* <div className="text-center lg:text-left">
+                    
+                </div> */}
+                <div className="card bg-base-100 w-full  shrink-0 shadow-2xl">
+                <h1 className="text-5xl font-bold">Apply Job and Good Luck</h1>
+                   
                     <form onSubmit={handleApply} className="card-body">
                         <div className="form-control">
                             <label className="label">
